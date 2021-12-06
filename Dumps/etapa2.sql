e71582360efecd8f00e87ae8a54c0abd2ca056ed
vitordorneless <vitordorneles@hotmail.com>
2021-12-05 22:17:19 -0300

    corrigindo sql

--- Dumps/etapa2.sql
+++ Dumps/etapa2.sql
@@ -4,7 +4,8 @@ datediff(a.to_date,a.from_date) as dias_trabalhados
 from dept_emp a
 inner join departments b on a.dept_no = b.dept_no
 inner join employees c on a.emp_no = c.emp_no
-where a.to_date is not null
-order by dias_trabalhados desc, nome_funcionario asc
+where a.to_date is not NULL
+GROUP BY b.dept_name
+order by dias_trabalhados desc, b.dept_name asc, nome_funcionario asc
 limit 10;
 
